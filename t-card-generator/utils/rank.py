2b98e78c349b47878e48ca7b29199f03d9a475c2
Lynxiie <helena.guirado@orange.fr>
2025-12-31 14:45:16 +0100

    Merge

---
+++ t-card-generator/utils/rank.py
@@ -0,0 +1,65 @@
+from sqlalchemy.orm.session import Session
+
+from forms import NewCookiesForm, UsedCookiesListForm
+from models import CookiesUsed, CookiesMonths, PokemonOwned
+
+
+def new_cookies(form: NewCookiesForm, character_id: int, session: Session):
+    """
+    Ajoute de nouveaux cookies
+    :param form: le formulaire d'ajout de cookies
+    :param character_id: l'id du personnage
+    :param session: la session BDD
+    """
+    cookies_win = int(form.win_cookies.data)
+
+    cookies_month = CookiesMonths(
+        character_id=character_id,
+        month=form.month.data,
+        win_cookies=cookies_win
+    )
+
+    session.add(cookies_month)
+    session.flush()
+    session.refresh(cookies_month)
+
+    new_cookies = []
+    for cookie_number in range(cookies_win):
+        new_cookies.append(CookiesUsed(
+            cookies_months_id=cookies_month.id
+        ))
+
+    session.add_all(new_cookies)
+    session.commit()
+
+
+def give_cookies(used_cookies_form: UsedCookiesListForm, session: Session):
+    """
+    Utilise des cookies
+    :param used_cookies_form: le formulaire d'utilisation des cookies
+    :param session: la session BDD
+    """
+    pokemon_name = set(cookies_forms.pokemon_name.data for cookies_forms in used_cookies_form.cookies_forms)
+    used_cookies_id = set(cookies_forms.used_cookies_id.data for cookies_forms in used_cookies_form.cookies_forms)
+
+    all_pokemon = session.query(PokemonOwned).filter(PokemonOwned.name.in_(pokemon_name)).all()
+    all_pokemon = {pokemon.name: pokemon for pokemon in all_pokemon}
+
+    used_cookies = session.query(CookiesUsed).filter(CookiesUsed.id.in_(used_cookies_id)).all()
+    used_cookies = {cookies.id: cookies for cookies in used_cookies}
+
+    for cookies_forms in used_cookies_form.cookies_forms:
+        pokemon_name = cookies_forms.pokemon_name.data
+        if not pokemon_name or pokemon_name == '0':
+            continue
+        pokemon = all_pokemon[pokemon_name]
+        cookies = used_cookies[int(cookies_forms.used_cookies_id.data)]
+
+        cookies.pokemon_name = pokemon.name
+        cookies.before_lvl = pokemon.level
+        pokemon.level += 1
+        cookies.after_lvl = pokemon.level
+
+        session.flush()
+
+    session.commit()
